235e302822d7d27f2732071a4d0f16bad9a1fba1
Gaurav <katariagaurav686@gmail.com>
2024-05-22 01:46:58 +0530

    DailyNews

---
+++ dailynews/lib/services/slider_data.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:dailynews/models/slider_model.dart';
+
+import 'package:http/http.dart' as http;
+
+class Sliders {
+  List<SliderModel> sliders = [];
+
+  Future<void> getSlider() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=030e79d463a44686b901707a99e2c975";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          SliderModel slidermodel = SliderModel(
+            title: element["title"],
+            description: element["description"],
+            url: element["url"],
+            urlToImage: element["urlToImage"],
+            content: element["content"],
+            author: element["author"],
+          );
+          sliders.add(slidermodel);
+        }
+      });
+    }
+  }
+}
